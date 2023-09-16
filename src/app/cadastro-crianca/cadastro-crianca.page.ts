0eedd2acbee5514836d53adeec9582a557c2f6c2
directionit <140574445+directionit@users.noreply.github.com>
2023-09-16 17:38:03 -0300

    envio de dados do formulário

--- src/app/cadastro-crianca/cadastro-crianca.page.ts
+++ src/app/cadastro-crianca/cadastro-crianca.page.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroCriancaPage implements OnInit {
 
+  crianca: Crianca = new Crianca(); 
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  salvar() {
+    console.log(this.crianca)
+  }
+
+}
+
+
+export class Crianca {
+   nome?: string; 
+  idade?: string;
+  responsavel?: string;
+
+
+  constructor(
+    nome?: string,
+    idade?: string,
+    responsavel?: string,
+
+  ) {
+    this.nome = nome;
+    this.idade = idade;
+    this.responsavel = responsavel
+
+  }
 }
