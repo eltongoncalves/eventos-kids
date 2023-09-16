dcdd3106a30a9acbe05b7089dd27fb7c3b2a3e1d
directionit <140574445+directionit@users.noreply.github.com>
2023-09-16 15:53:08 -0300

    criação inicial do projeto

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eventos-kids',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
